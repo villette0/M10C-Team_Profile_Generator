751d643a7ef7090268ee92de2dd16cbb0a068b86
violet978 <villette@live.com>
2022-07-07 13:47:35 -0700

    Added html mockup and styling

---
+++ index.js
@@ -0,0 +1,5 @@
+// Include packages needed for this application
+//The node modules of inquirer and file system are used in this application.
+const inquirer = require("inquirer");
+const fs = require("fs");
+const jest = require("jest");
